afa79b6ebf1a2a351232c09c080fb6879c4216be
CelesteAustin <yeche5180997@126.com>
2018-10-09 11:15:12 +0800

    updateProperty 0

--- sql.py
+++ sql.py
@@ -1314,12 +1314,12 @@ def updateProperty(PropertyID, Protocol, LastTxDBSerialNum=None):
       reorg = False
 
     if PropertyID == 0:
-      rawprop = {"name":"BTC", "blocktime":1231006505, "data":"The Times 03/Jan/2009 Chancellor on brink of second bailout for banks", "issuer":"Satoshi Nakamoto", "url":"http://www.bitcoin.org", "propertyid":0 ,"divisible": True}
+      rawprop = {"name":"HC", "blocktime":1231006505, "data":"The Times 03/Jan/2009 Chancellor on brink of second bailout for banks", "issuer":"Satoshi Nakamoto", "url":"https://h.cash/", "propertyid":0 ,"divisible": True}
       Issuer = rawprop['issuer']
       try:
-        r = requests.get('https://blockchain.info/q/totalbc')
-        amt=int(r.text)
-        rawprop['totaltokens'] = str(int(amt/1e8))
+        r = requests.get('https://hc-explorer.h.cash/supply', verify=False)
+        amt=r.json()["TOTALCIRCULATINGAMOUNT"]
+        rawprop['totaltokens'] = str(amt)
       except:
         pass
     else:
