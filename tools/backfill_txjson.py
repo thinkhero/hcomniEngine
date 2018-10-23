ba7eea7b8df3ea5372eb313d96e3ffe5af09e1c2
thinkhero <thinkerhero@126.com>
2018-10-23 14:27:50 +0800

    fix bug

--- tools/backfill_txjson.py
+++ tools/backfill_txjson.py
@@ -6,7 +6,7 @@ dbInit()
 rows = dbSelect("select * from transactions where txdbserialnum < 46631497;");
 for each in rows:
   #print each , gettransaction_MP(each[0])['result'] 
-  statement = "insert into txjson (txdbserialnum, txdata, protocol) values (" + str(each[2]) + ", \'" + json.dumps(gettransaction_MP(each[0])['result']) + "\', \'" + each[1] + "\')"
+  statement = "insert into txjson (txdbserialnum, txdata, protocol) values (" + str(each[3]) + ", \'" + json.dumps(gettransaction_MP(each[0])['result']) + "\', \'" + each[2] + "\')"
   print statement
   dbExecute(statement);
   dbCommit();
